5073506cd3240de05fac4705403abf8b508beffd
Ananthdev24796 <ananth.cr7@gmail.com>
2022-10-03 18:44:19 +0530

    Refactor:Removed extra methods

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -6,16 +6,11 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void checking_First_Name_Method_1(){
+    public void checking_First_Name_Method(){
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.checkFirstName("Honda");
         Assert.assertEquals(true,result);
     }
-    @Test
-    public void checking_First_Name_Method_2(){
-        UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.checkFirstName("honda");
-        Assert.assertEquals(true,result);
-    }
+
 
 }
