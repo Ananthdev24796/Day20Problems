fa45c4b4954db93dcdc257de8b07631dc35cd34a
Ananthdev24796 <ananth.cr7@gmail.com>
2022-10-03 21:01:48 +0530

    Added:usecase 6 for day 20 problems

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -6,29 +6,35 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void checking_First_Name_Method(){
+    public void checking_First_Name_Method() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.checkFirstName("Honda");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
+
     @Test
-    public void checking_Last_Name_Method(){
+    public void checking_Last_Name_Method() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.checkLastName("Amaze");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
+
     @Test
-    public void checking_EMail_Method(){
+    public void checking_EMail_Method() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.checkEMail("abc.xyz@bl.co.in");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
+
     @Test
-    public void checking_Phone_Number_Method(){
+    public void checking_Phone_Number_Method() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.checkPhoneNumber("91 9920456790");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
+
+
+
     @Test
     public void checking_Password_Method(){
         UserRegistration userRegistration = new UserRegistration();
@@ -36,5 +42,11 @@ public class UserRegistrationTest {
         Assert.assertEquals(true,result);
     }
 
+    @Test
+    public void checking_Password_Method_Atleast_One_Upper_Case(){
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.checkPasswordAtleastOneUpperCase("Password123");
+        Assert.assertEquals(true,result);
+    }
 
 }
