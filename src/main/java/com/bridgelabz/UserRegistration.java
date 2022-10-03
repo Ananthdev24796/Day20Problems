0891efc86284148d89e2243c09d952aa05e82e8d
Ananthdev24796 <ananth.cr7@gmail.com>
2022-10-03 18:40:50 +0530

    added:usecase 1 for day 20 problems

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserRegistration {
-   
+    /**
+     * UC1
+     * As a User need to enter a valid First Name
+     * First name starts with Cap and has minimum 3 characters
+     */
+
+        public static boolean checkFirstName(String fName){
+            String userRegistrationRegEx = "^[A-Z]{1}[a-z]{2,}";
+            Pattern patternObj = Pattern.compile(userRegistrationRegEx);
+
+            Matcher matcherObj = patternObj.matcher(fName);
+
+            return matcherObj.matches();
+        }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,7 +1,21 @@
 package com.bridgelabz;
 
 
+import org.junit.Assert;
+import org.junit.Test;
 
 public class UserRegistrationTest {
+    @Test
+    public void checking_First_Name_Method_1(){
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.checkFirstName("Honda");
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void checking_First_Name_Method_2(){
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.checkFirstName("honda");
+        Assert.assertEquals(true,result);
+    }
 
 }
